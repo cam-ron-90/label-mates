8892043d1ceeb38306c982dedc70250cbfdedf65
Cameron Melville <cam.t.melville@gmail.com>
2020-05-11 23:52:30 +1000

    artist and label search views setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,50 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  require 'discogs'
+
+  before_action do
+    @discogs = Discogs::Wrapper.new("Label Mates", session[:access_token])
+  end
+
+  before_action :authenticate
+  before_action :callback
+
+  skip_before_action :authenticate_user!
+
 
   def home
   end
+
+  def index
+  end
+
+  def authenticate
+    app_key      = ENV["DISCOGS_API_KEY"]
+    app_secret   = ENV["DISCOGS_API_SECRET"]
+    request_data = @discogs.get_request_token(app_key, app_secret,
+                     "http://127.0.0.1:3000/callback")
+
+    session[:request_token] = request_data[:request_token]
+
+    redirect_to request_data[:authorize_url]
+  end
+
+  def callback
+    request_token = session[:request_token]
+    verifier      = params[:oauth_verifier]
+    access_token  = @discogs.authenticate(request_token, verifier)
+
+    session[:request_token] = nil
+    session[:access_token]  = access_token
+
+    redirect_to :action => "index"
+  end
+
+  def artist_search
+  end
+
+  def label_search
+  end
+
+  private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pages do
+    collection do
+      get :authenticate
+      get :callback
+      get :artist_search
+      get :label_search
+    end
+  end
 end
